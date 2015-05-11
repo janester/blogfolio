fe11861a1e79e994ad894636919c599bd095ddf1
Jane Sternbach <janesternbach@gmail.com>
2015-05-11 00:46:04 -0700

    clicking the nav and style for nav

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,26 @@
 class HomeController < ApplicationController
   before_action :set_nav
   def index
+    @active = "home"
+  end
+
+  def resume
+    @active = "resume"
+  end
+
+  def services
+    @active = "services"
+  end
+
+  def portfolio
+    @active = "portfolio"
+  end
+
+  def blog
+    @active = "blog"
+  end
+
+  def contact
+    @active = "contact"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get "/home", to: "home#index"
+  get "/resume", to: "home#resume"
+  get "/services", to: "home#services"
+  get "/portfolio", to: "home#portfolio"
+  get "/blog", to: "home#blog"
+  get "/contact", to: "home#contact"
+
 end
