971650474da3e1844ff9dbcab0a63846a84e0a55
Jane Sternbach <janesternbach@gmail.com>
2015-05-31 02:26:03 -0700

    semicolons

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,5 @@
 $(document).on('ready page:load', function(){
-  var contentContainer = $('.main-content')
-  var newHeight = window.innerHeight - contentContainer.offset().top
+  var contentContainer = $('.main-content');
+  var newHeight = window.innerHeight - contentContainer.offset().top;
   contentContainer.css('height', newHeight + 'px');
 })
