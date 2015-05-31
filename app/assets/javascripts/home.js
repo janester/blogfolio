cfe9458ef5b8dd519131429a5580d14aa9462924
Jane Sternbach <janesternbach@gmail.com>
2015-05-31 02:25:33 -0700

    fix js bug for turbolinks

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).on('ready page:load', function(){
   var contentContainer = $('.main-content')
   var newHeight = window.innerHeight - contentContainer.offset().top
   contentContainer.css('height', newHeight + 'px');
