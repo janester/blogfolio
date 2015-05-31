cd00bfb3688f968fdc9a14932d0bcc6df772ca01
Jane Sternbach <janesternbach@gmail.com>
2015-05-31 00:48:10 -0700

    DRY up the controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,7 @@
 class HomeController < ApplicationController
-  before_action :set_nav
-  def index
-    @active = "home"
-  end
+  PAGES = %w(home resume services portfolio blog contact)
 
-  def resume
-    @active = "resume"
-  end
-
-  def services
-    @active = "services"
-  end
-
-  def portfolio
-    @active = "portfolio"
-  end
-
-  def blog
-    @active = "blog"
-  end
-
-  def contact
-    @active = "contact"
+  PAGES.each do |page|
+    define_method(page) {}
   end
 end
